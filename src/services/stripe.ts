2eb29dd5131aa42528676541f9364901dce49346
mauriani <maurianim@gmail.com>
2021-12-07 15:38:41 -0300

    ts

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,11 +1,20 @@
 import Stripe from "stripe";
 
-STRIPE_API_KEY=sk_test_51JzPVoCKbazxGPm0IapmVETO3SS958flnX2iQvQD70wFgt9GDCLjmQ1LxVSS8LWWNYRKdbP6eey72lNXYDJBR4M800BlvD2BJ4
-
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+/*export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
     name: "Ignews",
     version: "0.1.0",
   },
-});
+});*/
+
+export const stripe = new Stripe(
+  "sk_test_51JzPVoCKbazxGPm0IapmVETO3SS958flnX2iQvQD70wFgt9GDCLjmQ1LxVSS8LWWNYRKdbP6eey72lNXYDJBR4M800BlvD2BJ4",
+  {
+    apiVersion: "2020-08-27",
+    appInfo: {
+      name: "Ignews",
+      version: "0.1.0",
+    },
+  }
+);
